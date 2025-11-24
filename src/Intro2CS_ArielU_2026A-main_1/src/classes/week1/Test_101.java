51163e04d69be3048d468c134255494294180782
alex avidor <alexav@pelephone.co.il>
2025-11-24 21:20:42 +0200

    Initial commit

---
+++ src/Intro2CS_ArielU_2026A-main_1/src/classes/week1/Test_101.java
@@ -0,0 +1,14 @@
+/*
+ * output:
+Hello World!
+this is a nice String 15.899999999999999
+ */
+public class Test_101 {
+	public static void main(String[] args) {
+		double i = 3;
+		i = i * i;
+		System.out.println("Hello World!");
+		String msg = "this is a nice String ";
+		System.out.println(msg + ", i="+i);
+	}
+}
